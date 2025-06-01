01cb2ba04cafa191fd7ea55f54a53f048fef4876
Martin Koloseus <mkoloseus@posteo.de>
2025-06-01 21:13:20 +0200

    refactor(bitboard): use std::underlying_type

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 #include "types.hpp"
 
 using Bitboard = uint64_t;
 
 constexpr Bitboard FileBB(BoardFile file) {
     constexpr Bitboard FILE_A = (1 << 8) - 1;
-    return FILE_A << (static_cast<uint8_t>(file) * 8);
+    return FILE_A << (static_cast<std::underlying_type<BoardFile>::type>(file) * 8);
 }
 
 constexpr Bitboard RankBB(BoardRank rank) {
-    return 0x0101010101010101 << static_cast<uint8_t>(rank);
+    return 0x0101010101010101 << static_cast<std::underlying_type<BoardRank>::type>(rank);
 }
 
 constexpr Bitboard SquareBB(Square square) {
-    return 1ull << static_cast<uint8_t>(square);
+    return 1ull << static_cast<std::underlying_type<Square>::type>(square);
 }
 
