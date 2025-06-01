b84126f5906c315fef5624a91c422f56da70d6a3
Martin Koloseus <mkoloseus@posteo.de>
2025-06-01 21:06:06 +0200

    refactor(bitboard): just use uint8_t

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -7,14 +7,14 @@ using Bitboard = uint64_t;
 
 constexpr Bitboard FileBB(BoardFile file) {
     constexpr Bitboard FILE_A = (1 << 8) - 1;
-    return FILE_A << (static_cast<BoardFileEnumType>(file) * 8);
+    return FILE_A << (static_cast<uint8_t>(file) * 8);
 }
 
 constexpr Bitboard RankBB(BoardRank rank) {
-    return 0x0101010101010101 << static_cast<BoardRankEnumType>(rank);
+    return 0x0101010101010101 << static_cast<uint8_t>(rank);
 }
 
 constexpr Bitboard SquareBB(Square square) {
-    return 1ull << static_cast<SquareEnumType>(square);
+    return 1ull << static_cast<uint8_t>(square);
 }
 
