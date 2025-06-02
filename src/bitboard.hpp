164dc6243c15cdc5628e584975ef19c0af427cd1
Martin Koloseus <mk_alternativ@posteo.de>
2025-06-03 01:15:02 +0200

    feat(bitboard): Shift

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -19,6 +19,20 @@ constexpr Bitboard SquareBB(Square square) {
     return 1ull << static_cast<std::underlying_type<Square>::type>(square);
 }
 
+template <Direction dir>
+constexpr Bitboard Shift(Bitboard bb) {
+    constexpr bool ShiftsUp     = dir == Direction::UP || dir == Direction::UP_LEFT || dir == Direction::UP_RIGHT;
+    constexpr bool ShiftsDown   = dir == Direction::DOWN || dir == Direction::DOWN_LEFT || dir == Direction::DOWN_RIGHT;
+    constexpr bool ShiftsRight  = dir == Direction::RIGHT || dir == Direction::UP_RIGHT || dir == Direction::DOWN_RIGHT;
+    constexpr bool ShiftsLeft   = dir == Direction::LEFT || dir == Direction::UP_LEFT || dir == Direction::DOWN_LEFT;
+    constexpr int shift         = (ShiftsUp ? 8 : 0) + (ShiftsDown ? -8 : 0) + (ShiftsRight ? 1 : 0) + (ShiftsLeft ? -1 : 0);
+    if constexpr (shift >= 0)   bb <<= shift;
+    else                        bb >>= -shift;
+    if constexpr (ShiftsRight)  bb &= ~RankBB(BoardRank::R1);
+    if constexpr (ShiftsLeft)   bb &= ~RankBB(BoardRank::R8);
+    return bb;
+}
+
 inline Square Lsb(Bitboard bb) {
     assert(bb);
 
