13b7009df1fbc901db0ee3a2e7183ad855c8bf31
Martin Koloseus <mk_alternativ@posteo.de>
2025-06-05 00:46:26 +0200

    feat(castling_rights)

---
+++ src/castling_rights.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+#include "types.hpp"
+
+class CastlingRights {
+public:
+    static constexpr uint8_t WHITE_QUEENSIDE = 1;
+    static constexpr uint8_t WHITE_KINGSIDE = 2;
+    static constexpr uint8_t BLACK_QUEENSIDE = 4;
+    static constexpr uint8_t BLACK_KINGSIDE = 8;
+    static constexpr uint8_t WHITE = WHITE_QUEENSIDE | WHITE_KINGSIDE;
+    static constexpr uint8_t BLACK = BLACK_QUEENSIDE | BLACK_KINGSIDE;
+    
+    CastlingRights() { mRights = WHITE | BLACK; }
+    CastlingRights(uint8_t rights) { mRights = rights; }
+    operator uint8_t&() { return mRights; }
+
+    template <Color color> void ForbidCastling() { 
+        static_assert(IsValid(color));
+        mRights &= color == Color::White ? ~WHITE : ~BLACK; 
+    }
+
+    template <Color color> void ForbidCastlingQueenside() { 
+        static_assert(IsValid(color));
+        mRights &= color == Color::White ? ~WHITE_QUEENSIDE : ~BLACK_QUEENSIDE; 
+    }
+
+    template <Color color> void ForbidCastlingKingside() { 
+        static_assert(IsValid(color));
+        mRights &= color == Color::White ? ~WHITE_KINGSIDE : ~BLACK_KINGSIDE; 
+    }
+
+    template <Color color> bool CanCastleQueenside() { 
+        static_assert(IsValid(color));
+        return (mRights & (color == Color::White ? WHITE_QUEENSIDE : BLACK_QUEENSIDE)); 
+    }
+
+    template <Color color> bool CanCastleKingside() { 
+        static_assert(IsValid(color));
+        return (mRights & (color == Color::White ? WHITE_KINGSIDE : BLACK_KINGSIDE)); 
+    }
+
+
+private:
+    uint8_t mRights;
+};
