c2c44d494e3b9a49af8cf6f5686ff5e5fce210b6
Martin Koloseus <mkoloseus@posteo.de>
2025-10-09 00:04:02 +0200

    feat(Position): InitFromFEN

---
+++ src/position.cpp
@@ -0,0 +1,100 @@
+#include "position.hpp"
+#include <stdexcept>
+#include <cctype>
+#include <string>
+
+void Position::InitFromFEN(const char* fen) {
+    fen = InitFromFEN_PiecePosition(fen);
+    fen = InitFromFEN_ExpectSpace(fen);
+    fen = InitFromFEN_SideToMove(fen);
+    fen = InitFromFEN_ExpectSpace(fen);
+    fen = InitFromFEN_CastlingRights(fen);
+    fen = InitFromFEN_ExpectSpace(fen);
+    fen = InitFromFEN_ReversableHalfMoves(fen);
+    fen = InitFromFEN_ExpectSpace(fen);
+    fen = InitFromFEN_MoveNum(fen);
+}
+
+const char* Position::InitFromFEN_PiecePosition(const char* fen) {
+    BoardRank rank = BoardRank::R8;
+    BoardFile file = BoardFile::A;
+    for (; *fen && *fen != ' '; ++fen) {
+        if (*fen == '/') {
+            if (file <= BoardFile::H) 
+                throw std::invalid_argument("Illegal fen: '/' before end of rank");
+            if (rank == BoardRank::R1)
+                throw std::invalid_argument("Illegal fen: '/' after last rank");
+            --rank; file = BoardFile::A;
+        } 
+        else if (file > BoardFile::H) {
+            throw std::invalid_argument("Illegal fen: rank already full, '/' expected");
+        }
+        else if ('1' <= *fen && *fen <= '8') {
+            file += (*fen - '0');
+            if (file > BoardFile::H + 1)
+                throw std::invalid_argument("Illegal fen: skipped past end of rank");
+        }
+        else { // piece
+            Color color = std::isupper(*fen) ? Color::White : Color::Black;
+            PieceType type = PieceType::None;
+            switch (std::toupper(*fen)) {
+                case 'K': type = PieceType::King; break;
+                case 'Q': type = PieceType::Queen; break;
+                case 'R': type = PieceType::Rook; break;
+                case 'N': type = PieceType::Knight; break;
+                case 'B': type = PieceType::Bishop; break;
+                case 'P': type = PieceType::Pawn; break;
+                default: throw std::invalid_argument("Illegal fen: illegal piece type");
+            }
+            AddPiece(MakePiece(color, type), MakeSquare(file, rank));
+        }
+    }
+    return fen;
+}
+
+const char* Position::InitFromFEN_SideToMove(const char* fen) {
+    if (!(*fen == 'w' || *fen == 'b')) throw std::invalid_argument("Illegal fen: expected 'w' or 'b' as active color");
+    if (*fen == 'w') mSideToMove = Color::White;
+    else mSideToMove = Color::Black;
+    return fen + 1;
+}
+
+const char* Position::InitFromFEN_CastlingRights(const char* fen) {
+    mCastlingRights = CastlingRights::NONE;
+    if (*fen == '-') return fen + 1;
+    if (*fen == 'K') {
+        mCastlingRights.AllowCastlingKingside<Color::White>();
+        ++fen;
+    }
+    if (*fen == 'Q') {
+        mCastlingRights.AllowCastlingQueenside<Color::White>();
+        ++fen;
+    }
+    if (*fen == 'k') {
+        mCastlingRights.AllowCastlingKingside<Color::Black>();
+        ++fen;
+    }
+    if (*fen == 'q') {
+        mCastlingRights.AllowCastlingQueenside<Color::Black>();
+        ++fen;
+    }
+    if (mCastlingRights == CastlingRights::NONE) throw std::invalid_argument("Illegal fen: invalid castling rights");
+    return fen;
+}
+
+const char* Position::InitFromFEN_ReversableHalfMoves(const char* fen) {
+    mReversableHalfMovesCnt = std::stoul(fen);
+    while (*fen && *fen != ' ') ++fen;
+    return fen;
+}
+
+const char* Position::InitFromFEN_MoveNum(const char* fen) {
+    mMoveNum = std::stoul(fen);
+    while (*fen && *fen != ' ') ++fen;
+    return fen;
+}
+
+const char* Position::InitFromFEN_ExpectSpace(const char* fen) {
+    if (*fen != ' ') throw std::invalid_argument("Illegal fen: fen ends before castling rights color");
+    return fen + 1;
+}
--- src/position.hpp
+++ src/position.hpp
@@ -4,14 +4,32 @@
 #include "bitboard.hpp"
 #include "castling_rights.hpp"
 
+#include <string>
+
 class Position {
 public:
+    Position() { InitFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); }
+    explicit Position(const char* fen) { InitFromFEN(fen); }
+    explicit Position(const std::string& fen) { InitFromFEN(fen.c_str()); }
+    
 
 private:
-    Bitboard mPiecesBB[COLOR_NUM][PIECE_TYPE_NUM];
-    Piece mBoard[SQUARE_NUM];
-    Color mSideToMove;
-    CastlingRights mCastlingRights;
-    Square mEnPassant;
-    uint32_t mReversableMovesCnt;
+    Bitboard mPiecesBB[COLOR_NUM][PIECE_TYPE_NUM]   = { (Bitboard)0 };
+    Piece mBoard[SQUARE_NUM]                        = { Piece::None };
+    Color mSideToMove                               = Color::White;
+    CastlingRights mCastlingRights                  = CastlingRights::ALL;
+    Square mEnPassant                               = Square::None;
+    uint32_t mReversableHalfMovesCnt                = 0;
+    uint32_t mMoveNum                               = 1;
+
+    void InitFromFEN(const char* fen);
+    const char* InitFromFEN_PiecePosition(const char* fen);
+    const char* InitFromFEN_SideToMove(const char* fen);
+    const char* InitFromFEN_CastlingRights(const char* fen);
+    const char* InitFromFEN_ReversableHalfMoves(const char* fen);
+    const char* InitFromFEN_MoveNum(const char* fen);
+    const char* InitFromFEN_ExpectSpace(const char* fen);
+    
+
+    void AddPiece(Piece piece, Square square);
 };
