f86d510d35772f92f5b8af36e3ec8951bb771623
Martin Koloseus <mkoloseus@posteo.de>
2025-10-13 04:11:37 +0200

    test(perft): Tests & PrintPerftPerMove

--- src/perft.cpp
+++ src/perft.cpp
@@ -106,25 +106,52 @@ void TestPerft(const char* fen, int depth, const PerftResult& correctPerftResult
     }
 }
 
+void PrintPerftPerMove(Position& pos, int depth) {
+    MoveList list(pos);
+    uint64_t total = 0;
+    for (Move move : list) {
+        pos.DoMove(move);
+        uint64_t perftResult = PerftSimple(pos, depth - 1);
+        total += perftResult;
+        Square from = move.GetFrom();
+        Square to = move.GetTo();
+        std::cout 
+            << char('a' + ToInt(FileOf(from))) << char('1' + ToInt(RankOf(from)))
+            << char('a' + ToInt(FileOf(to))) << char('1' + ToInt(RankOf(to)))
+            << ": " << perftResult
+            << std::endl;
+        pos.UndoMove();
+    }
+    std::cout << "Total: " << total << std::endl;
+}
+
 int main() {
     BB::Init();
-    TestPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, {
-        2039,
-        351,
-        1 ,
-        91,
+
+    TestPerftSimple("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 7, 3195901860);
+    TestPerft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, {
+        119060324,
+        2812008,
+        5248,
         0,
-        3,
-        0 	
+        0,
+        809099,
+        46	
     });
-    TestPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, {
-        4085603,
-        757163,
-        1929,
-        128013,
-        15172,
-        25523,
-        6 	
+
+    TestPerftSimple("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 6, 8031647685);
+    TestPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5, {
+        193690690,
+        35043416,
+        73365,
+        4993637,
+        8392,
+        3309887,
+        2645	
     });
-    // TestPerftSimple("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3'894'594);
+
+    TestPerftSimple("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 8, 3009794393);
+    TestPerftSimple("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 6, 706045033);
+    TestPerftSimple("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 5, 89941194);
+    TestPerftSimple("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6, 6923051137);
 }
