0b4b288c22c78451d674442934ddc77089690214
Martin Koloseus <mkoloseus@posteo.de>
2025-10-28 21:13:09 +0100

    feat(TranspositionTable): WriteEntry, IndexOf, ...

--- src/transposition_table.cpp
+++ src/transposition_table.cpp
@@ -4,4 +4,5 @@ TranspositionTable::TranspositionTable(uint64_t size) {
     mTable.resize(size, {
 
     });
+    currentAge = 0;
 }
--- src/transposition_table.hpp
+++ src/transposition_table.hpp
@@ -24,9 +24,40 @@ public:
     };
 
     explicit TranspositionTable(uint64_t size);
-    void AddEntry(ZobristHash hash, Move bestMove, Score score, uint16_t depth, Entry::Type type);
+    void AddEntry(Entry entry);
+    Entry GetEntry(ZobristHash hash);
+    void NewSearch();
 
 private:
     std::vector<Entry> mTable;
+    uint8_t currentAge;
+
+    std::size_t IndexOf(ZobristHash hash);
+    bool WriteEntry(Entry entry);
 
 };
+
+inline void TranspositionTable::AddEntry(Entry entry) {
+    // TODO: slow because division?
+    std::size_t index = IndexOf(entry.hash);
+    Entry& stored = mTable[index];
+    if (stored.hash != 0) {
+        if (stored.age == currentAge) {
+            if (stored.type != Entry::Type::PV && entry.type == Entry::Type::PV) 
+        }
+    }
+    stored = entry;
+}
+
+inline TranspositionTable::Entry TranspositionTable::GetEntry(ZobristHash hash) {
+    return mTable[IndexOf(hash)];
+}
+
+inline void TranspositionTable::NewSearch() {
+    ++currentAge;
+}
+
+inline std::size_t TranspositionTable::IndexOf(ZobristHash hash) {
+    // TODO: Slow?
+    return hash % mTable.size();
+}
